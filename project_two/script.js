121abcfbb03d9582118c03befce8a091d195d786
TimothyJHW <timothywilliamsterry@gmail.com>
2022-09-21 23:08:10 +0100

    added change font, padding, fixed alignment, and change case (still not working)

--- project_two/script.js
+++ project_two/script.js
@@ -14,12 +14,72 @@ const addNavEntryButton = document.getElementById('addNavEntry');
 const htmlCodeSection = document.getElementById('navbar-code-html');
 const cssCodeSection = document.getElementById('navbar-code-css');
 const alignment = document.getElementById('align');
-const csspadding = document.getElementById('padding');
-var cssPanel = {
-    display: "inline-block",
-    justifyContent: "right",
-    padding: 0,
-}
+const textCase = document.getElementById('case');
+const cssPadding = document.getElementById('padding');
+const cssfontFamily = document.getElementById('fontFamily');
+
+const windowsFontList = [
+'Arial',
+'Arial Black',
+'Bahnschrift',
+'Calibri',
+'Cambria',
+'Cambria Math',
+'Candara',
+'Comic Sans MS',
+'Consolas',
+'Constantia',
+'Corbel',
+'Courier New',
+'Ebrima',
+'Franklin Gothic Medium',
+'Gabriola',
+'Gadugi',
+'Georgia',
+'HoloLens MDL2 Assets',
+'Impact',
+'Ink Free',
+'Javanese Text',
+'Leelawadee UI',
+'Lucida Console',
+'Lucida Sans Unicode',
+'Malgun Gothic',
+'Marlett',
+'Microsoft Himalaya',
+'Microsoft JhengHei',
+'Microsoft New Tai Lue',
+'Microsoft PhagsPa',
+'Microsoft Sans Serif',
+'Microsoft Tai Le',
+'Microsoft YaHei',
+'Microsoft Yi Baiti',
+'MingLiU-ExtB',
+'Mongolian Baiti',
+'MS Gothic',
+'MV Boli',
+'Myanmar Text',
+'Nirmala UI',
+'Palatino Linotype',
+'Segoe MDL2 Assets',
+'Segoe Print',
+'Segoe Script',
+'Segoe UI',
+'Segoe UI Historic',
+'Segoe UI Emoji',
+'Segoe UI Symbol',
+'SimSun',
+'Sitka',
+'Sylfaen',
+'Symbol',
+'Tahoma',
+'Times New Roman',
+'Trebuchet MS',
+'Verdana',
+'Webdings',
+'Wingdings',
+'Yu Gothic',
+]
+
 
 // General Variable Definitions
 
@@ -42,7 +102,11 @@ copyButtonHtml.addEventListener("click", copyToClipboard)
 copyButtonCss.addEventListener("click", copyToClipboard)
 // trashButton.addEventListener("click",deleteNavbarEntry);
 alignment.addEventListener("click",align);
-csspadding.addEventListener("click",padding);
+textCase.addEventListener("click",changeCase);
+cssPadding.addEventListener("click",padding);
+cssfontFamily.addEventListener("click",fontFamily);
+
+
 
 function align (e) {
 
@@ -61,6 +125,11 @@ function align (e) {
         cssCodeString = "justify-content : right;"
         document.getElementById("cssAlignment").innerText = cssCodeString
     }
+    else if (e.target.getAttribute("id")=="fullwidth"){
+        navbarArea.style.justifyContent = "space-between";
+        cssCodeString = "justify-content : space-between;"
+        document.getElementById("cssAlignment").innerText = cssCodeString
+    }
 }
 
 function updateCssPanel () {
@@ -69,8 +138,34 @@ function updateCssPanel () {
 
 
 function padding(e) {
-    target= e.target
-    
+    targetValue = (e.target.value/3) + "rem"
+    navbarArea.style.padding = targetValue
+    cssCodeString = "padding : " + targetValue
+    document.getElementById("cssPadding").innerText = cssCodeString
+}
+function fontFamily(e) {
+    targetValue = e.target.value
+    navbarArea.style.fontFamily = targetValue;
+    cssCodeString = "font-family: " + targetValue
+    document.getElementById("cssfontFamily").innerText = cssCodeString
+}
+
+function changeCase (e) {
+    if(e.target.getAttribute("id")=="upper"){
+        navbarArea.style.textTransform = "uppercase;"
+        cssCodeString = "text-transform : uppercase;"
+        document.getElementById("cssAlignment").innerText = cssCodeString
+    }
+    else if (e.target.getAttribute("id")=="lower"){
+        navbarArea.style.textTransform = "lowercase;"
+        cssCodeString = "text-transform : lowercase;"
+        document.getElementById("cssAlignment").innerText = cssCodeString
+    }
+    else if (e.target.getAttribute("id")=="capitalize"){
+        navbarArea.style.textTransform = "capitalize";
+        cssCodeString = "text-transform : capitalize;"
+        document.getElementById("cssAlignment").innerText = cssCodeString
+    }
 }
 
 
@@ -100,6 +195,19 @@ function addNavItem(event) {
     htmlCodeSection.appendChild(htmlCodeStringElement)
 
 }
+
+//loads the array of fonts into the select html elements
+
+function loadFontList(list) {
+    fontOptions = document.getElementById("fontFamily");
+    for (i of list) {
+        newOption = document.createElement("option");
+        newOption.innerHTML = i;
+        newOption.setAttribute("value",i);
+        fontOptions.appendChild(newOption)
+    }
+}
+
 
 function removeNavItem(event) {
     navbarList.pop();
