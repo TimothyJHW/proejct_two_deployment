9399fe2b233337428a9186c7001916366e732bd2
TimothyJHW <timothywilliamsterry@gmail.com>
2022-09-29 15:41:26 +0100

    Final

--- project_two/script.js
+++ project_two/script.js
@@ -1,7 +1,5 @@
 //Assign buttons to variables
 
-const plusButton = document.getElementById("plusButton");
-const minusButton = document.getElementById("minusButton");
 const trashButton = document.getElementById("trashButton");
 
 const navbarArea = document.getElementById("navbar-mockup-list");
@@ -19,7 +17,6 @@ const cssCase = document.getElementById('cssCase');
 const csscolor = document.getElementById('color');
 const cssbackgroundColor = document.getElementById('cssCase');
 
-
 // the text value of the font slider
 
 document.querySelector("p > span").innerText = document.getElementById("fontsizerange").value;
@@ -28,34 +25,20 @@ const windowsFontList = [
 'Arial','Arial Black','Bahnschrift','Calibri','Cambria','Cambria Math','Candara','Comic Sans MS','Consolas','Constantia','Corbel','Courier New','Ebrima','Franklin Gothic Medium','Gabriola','Gadugi','Georgia','HoloLens MDL2 Assets','Impact','Ink Free','Javanese Text','Leelawadee UI','Lucida Console','Lucida Sans Unicode','Malgun Gothic','Marlett','Microsoft Himalaya','Microsoft JhengHei','Microsoft New Tai Lue','Microsoft PhagsPa','Microsoft Sans Serif','Microsoft Tai Le','Microsoft YaHei','Microsoft Yi Baiti','MingLiU-ExtB','Mongolian Baiti','MS Gothic','MV Boli','Myanmar Text','Nirmala UI','Palatino Linotype','Segoe MDL2 Assets','Segoe Print','Segoe Script','Segoe UI','Segoe UI Historic','Segoe UI Emoji','Segoe UI Symbol','SimSun','Sitka','Sylfaen','Symbol','Tahoma','Times New Roman','Trebuchet MS','Verdana','Webdings','Wingdings','Yu Gothic',
 ]
 
-
 // General Variable Definitions
 
-var navbarList = [];
 var navbarListNum = 0;
-htmlCopytoClipboard = {}
-cssCopytoClipboard = {}
-
-
 
 //Add listeners to button variables
 
 document.addEventListener("DOMContentLoaded",init);
 addNavEntryButton.addEventListener('click', addNavItem);
-
-// minusButton.addEventListener("click",removeNavItem);
-
 copyButtonHtml.addEventListener("click", copyToClipboard)
 copyButtonCss.addEventListener("click", copyToClipboard)
-
-// trashButton.addEventListener("click",deleteNavbarEntry);
-
 alignment.addEventListener("click",align);
 textCase.addEventListener("click",changeCase);
-// textPadding.forEach() addEventListener("click", padding);
+cssfontFamily.addEventListener("click",fontFamily)
 
-
-cssfontFamily.addEventListener("click",fontFamily);
 document.getElementById("fontsizerange").addEventListener("change",fontsizechange);
 document.getElementById("opacityrange").addEventListener("change",opacitychange);
 document.getElementById("txtcolor").addEventListener("change",colorPicker);
@@ -75,28 +58,37 @@ function init() {
     }
 }
 
-
 function whichSpacing(e) {
-    if(e.srcElement.id =="paddingT"){
-    targetValue = (e.target.value/3) + "rem";
+    if(e.target.id =="paddingT"){
+    targetValue = e.target.value + "px";
     navbarArea.style.padding = targetValue;
     cssCodeString = "padding : " + targetValue;
     document.getElementById("cssPadding").innerText = cssCodeString;
     }
-    else if (e.srcElement.id =="paddingR") {
-        targetValue = (e.target.value/3) + "rem"
-        navbarArea.style.paddingRight = targetValue;
-        cssCodeString = "padding : " + targetValue;
-        document.getElementById("cssPadding").innerText = cssCodeString;
+    else if (e.target.id =="paddingR") {
+        targetValue = e.target.value + "px"
+        listElements = document.querySelectorAll("#navbar-mockup-list li");
+        for (i of listElements){
+            i.style.paddingLeft = "";
+            i.style.paddingRight = targetValue;
+        }
+        cssCodeString = "padding-right : " + targetValue;
+        document.getElementById("cssPaddingLR").innerText = cssCodeString;
+        document.getElementById("paddingL").value = 1;
     }
-    else if (e.srcElement.id =="paddingL") {
-        targetValue = (e.target.value/3) + "rem";
-        navbarArea.style.paddingLeft = targetValue;
-        cssCodeString = "padding : " + targetValue;
-        document.getElementById("cssPadding").innerText = cssCodeString;
+    else if (e.target.id =="paddingL") {
+        targetValue = e.target.value + "px";
+        listElements = document.querySelectorAll("#navbar-mockup-list li");
+        for (i of listElements){
+            i.style.paddingRight = "";
+            i.style.paddingLeft = targetValue;
+        }
+        cssCodeString = "padding-left : " + targetValue;
+        document.getElementById("cssPaddingLR").innerText = cssCodeString;
+        document.getElementById("paddingR").value = 1;
     }
-    else if (e.srcElement.id =="letterS") {
-        targetValue = (e.target.value/3) + "rem";
+    else if (e.target.id =="letterS") {
+        targetValue = e.target.value + "px";
         navbarArea.style.letterSpacing = targetValue;
         cssCodeString = "letter-spacing : " + targetValue;
         document.getElementById("cssletterSpacing").innerText = cssCodeString;
@@ -115,7 +107,6 @@ function colorPicker(e) {
         document.getElementById("csscolor").innerText = cssCodeString;
     }
 }
-
 
 // text-alignment function
 
@@ -139,13 +130,16 @@ function align (e) {
         navbarArea.style.justifyContent = "space-between";
         cssCodeString = "justify-content : space-between;"
         document.getElementById("cssAlignment").innerText = cssCodeString;
+        listElements = document.querySelectorAll("#navbar-mockup-list li");
+        for (i of listElements) {
+        i.style.paddingRight = "";
+        i.style.paddingLeft = "";
+        document.getElementById("cssPaddingLR").innerText = "padding-left: 0px";
+        document.getElementById("paddingR").value = 1;
+        document.getElementById("paddingL").value = 1;
+        }
     }
 }
-
-function updateCssPanel () {
-
-}
-
 
 // change font function
 
@@ -191,21 +185,18 @@ function opacitychange(e) {
     document.getElementById("cssopacity").innerText = cssCodeString;
 }
 
-
-
 // Function add and delete navbar list entries
 
 function addNavItem(e) {
     e.preventDefault();
     if (navbarListNum < 5) {
         newNavItem = document.getElementById('navbar-entries').value;
-        navbarList.push([newNavItem,navbarListNum]);
         listItem = document.createElement("li");
         listItem.innerHTML = newNavItem;
         navbarArea.appendChild(listItem);
         htmlCodeString = "<li>"+newNavItem+"</li>";
 
-        // On the screen it appears as an "li" element behind the scenes it is a "p" element
+// On the screen it appears as an "li" element behind the scenes it is a "p" element
 
         htmlCodeStringElement = document.createElement("p");
         htmlCodeStringElement.setAttribute("draggable",true);
@@ -233,15 +224,14 @@ function attachDragListeners(el) {
 }
 
 function updateNavItems(e) {
-    e.target.classList.remove("dragover");
-    //deletion drop process
+    //deletion drop process     
+    e.target.classList.remove("dragover");   
     if(e.target.id == "trash"){
         itemtoDelete = document.getElementById(`${dragStartId}`)
         // this is to delete the entry in the mockup area. Gets nodelist from the ul id of navbar-mockup-list
         mockupitemtoDelete = document.querySelectorAll("#navbar-mockup-list li")
         itemtoDelete.remove();
         mockupitemtoDelete[dragStartId].remove();
-        navbarList.splice(dragStartId,1);
         navbarListNum --;
         reIndexEntries()
     e.target.classList.remove("dragover");}
@@ -254,15 +244,7 @@ function updateNavItems(e) {
         changeTo.innerHTML = changeFrom.innerHTML
         changeFrom.innerHTML = ctDuplicate;
         reIndexEntries();
-        mockupList = document.querySelectorAll("#navbar-mockup-list li");
-        iter =0 
-        // for (i of mockupList) {
-        //     i.innerHTML = codeList[iter].innerText;
-        //     iter++
-        // }
-        for (i=0;i<mockupList.length;i++) {
-            mockupList[i].innerHTML = codeList[i].innerText;}
-
+        reloadNavbarMockup(); 
     }
 }
 
@@ -273,6 +255,17 @@ function reIndexEntries(){
         i.id = iter.toString();
         iter++;
     }
+}
+
+// takes an changes in the code area and replicates them in the nodelist in the mockup area.
+function reloadNavbarMockup () {
+    const mockupList = document.querySelectorAll("#navbar-mockup-list li");
+    const codeList = document.querySelectorAll("#navbar-code-html p");
+        for (i=0; i < mockupList.length; i++) {
+            //removes the "li" tags embedded in the text of the p tag in the code area
+            clistOne = codeList[i].textContent.slice(4,-5)
+            mockupList[i].innerHTML = clistOne;
+        }
 }
 
 //loads the array of fonts into the select html elements
@@ -287,19 +280,6 @@ function loadFontList(list) {
     }
 }
 
-
-// function removeNavItem(e) {
-//     navbarList.pop();
-//     console.log(event.srcElement);
-//     console.log(navbarList)
-//     navbarArea.removeChild(navbarArea.lastElementChild)
-//     if (navbarListNum > 0) {navbarListNum--;}
-// }
-
-
-// function deleteNavbarEntry(event){
-//     console.log("del working")
-// }
 
 // copy to clipboard function
 
