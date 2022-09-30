161d1b6ed1826c941aded8c179ef220f45adbd31
TimothyJHW <timothywilliamsterry@gmail.com>
2022-09-30 19:50:08 +0100

    sent through a linter for all 3 languages

--- project_two/script.js
+++ project_two/script.js
@@ -5,25 +5,25 @@ const trashButton = document.getElementById("trashButton");
 const navbarArea = document.getElementById("navbar-mockup-list");
 const copyButtonHtml = document.getElementById("copy-html");
 const copyButtonCss = document.getElementById("copy-css");
-const addNavEntryButton = document.getElementById('addNavEntry');
-const htmlCodeSection = document.getElementById('navbar-code-html');
-const cssCodeSection = document.getElementById('navbar-code-css');
-const alignment = document.getElementById('align');
-const textPadding = document.getElementsByClassName('padding');
-const textCase = document.getElementById('case');
-const cssPadding = document.getElementById('cssPadding');
-const cssfontFamily = document.getElementById('fontFamily');
-const cssCase = document.getElementById('cssCase');
-const csscolor = document.getElementById('color');
-const cssbackgroundColor = document.getElementById('cssCase');
+const addNavEntryButton = document.getElementById("addNavEntry");
+const htmlCodeSection = document.getElementById("navbar-code-html");
+const cssCodeSection = document.getElementById("navbar-code-css");
+const alignment = document.getElementById("align");
+const textPadding = document.getElementsByClassName("padding");
+const textCase = document.getElementById("case");
+const cssPadding = document.getElementById("cssPadding");
+const cssfontFamily = document.getElementById("fontFamily");
+const cssCase = document.getElementById("cssCase");
+const csscolor = document.getElementById("color");
+const cssbackgroundColor = document.getElementById("cssCase");
 
 // the text value of the font slider
 
 document.querySelector("p > span").innerText = document.getElementById("fontsizerange").value;
 
 const windowsFontList = [
-'Arial','Arial Black','Bahnschrift','Calibri','Cambria','Cambria Math','Candara','Comic Sans MS','Consolas','Constantia','Corbel','Courier New','Ebrima','Franklin Gothic Medium','Gabriola','Gadugi','Georgia','HoloLens MDL2 Assets','Impact','Ink Free','Javanese Text','Leelawadee UI','Lucida Console','Lucida Sans Unicode','Malgun Gothic','Marlett','Microsoft Himalaya','Microsoft JhengHei','Microsoft New Tai Lue','Microsoft PhagsPa','Microsoft Sans Serif','Microsoft Tai Le','Microsoft YaHei','Microsoft Yi Baiti','MingLiU-ExtB','Mongolian Baiti','MS Gothic','MV Boli','Myanmar Text','Nirmala UI','Palatino Linotype','Segoe MDL2 Assets','Segoe Print','Segoe Script','Segoe UI','Segoe UI Historic','Segoe UI Emoji','Segoe UI Symbol','SimSun','Sitka','Sylfaen','Symbol','Tahoma','Times New Roman','Trebuchet MS','Verdana','Webdings','Wingdings','Yu Gothic',
-]
+"Arial","Arial Black","Bahnschrift","Calibri","Cambria","Cambria Math","Candara","Comic Sans MS","Consolas","Constantia","Corbel","Courier New","Ebrima","Franklin Gothic Medium","Gabriola","Gadugi","Georgia","HoloLens MDL2 Assets","Impact","Ink Free","Javanese Text","Leelawadee UI","Lucida Console","Lucida Sans Unicode","Malgun Gothic","Marlett","Microsoft Himalaya","Microsoft JhengHei","Microsoft New Tai Lue","Microsoft PhagsPa","Microsoft Sans Serif","Microsoft Tai Le","Microsoft YaHei","Microsoft Yi Baiti","MingLiU-ExtB","Mongolian Baiti","MS Gothic","MV Boli","Myanmar Text","Nirmala UI","Palatino Linotype","Segoe MDL2 Assets","Segoe Print","Segoe Script","Segoe UI","Segoe UI Historic","Segoe UI Emoji","Segoe UI Symbol","SimSun","Sitka","Sylfaen","Symbol","Tahoma","Times New Roman","Trebuchet MS","Verdana","Webdings","Wingdings","Yu Gothic"
+];
 
 // General Variable Definitions
 
@@ -32,12 +32,12 @@ var navbarListNum = 0;
 //Add listeners to button variables
 
 document.addEventListener("DOMContentLoaded",init);
-addNavEntryButton.addEventListener('click', addNavItem);
-copyButtonHtml.addEventListener("click", copyToClipboard)
-copyButtonCss.addEventListener("click", copyToClipboard)
+addNavEntryButton.addEventListener("click", addNavItem);
+copyButtonHtml.addEventListener("click", copyToClipboard);
+copyButtonCss.addEventListener("click", copyToClipboard);
 alignment.addEventListener("click",align);
 textCase.addEventListener("click",changeCase);
-cssfontFamily.addEventListener("click",fontFamily)
+cssfontFamily.addEventListener("click",fontFamily);
 
 document.getElementById("fontsizerange").addEventListener("change",fontsizechange);
 document.getElementById("opacityrange").addEventListener("change",opacitychange);
@@ -66,7 +66,7 @@ function whichSpacing(e) {
     document.getElementById("cssPadding").innerText = cssCodeString;
     }
     else if (e.target.id =="paddingR") {
-        targetValue = e.target.value + "px"
+        targetValue = e.target.value + "px";
         listElements = document.querySelectorAll("#navbar-mockup-list li");
         for (i of listElements){
             i.style.paddingLeft = "";
@@ -113,22 +113,22 @@ function colorPicker(e) {
 function align (e) {
     if(e.target.getAttribute("id")=="left"){
         navbarArea.style.justifyContent = "left";
-        cssCodeString = "justify-content : left;"
+        cssCodeString = "justify-content : left";
         document.getElementById("cssAlignment").innerText = cssCodeString;
     }
     else if (e.target.getAttribute("id")=="center"){
         navbarArea.style.justifyContent = "center";
-        cssCodeString = "justify-content : center;"
+        cssCodeString = "justify-content : center;";
         document.getElementById("cssAlignment").innerText = cssCodeString;
     }
     else if (e.target.getAttribute("id")=="right"){
         navbarArea.style.justifyContent = "right";
-        cssCodeString = "justify-content : right;"
+        cssCodeString = "justify-content : right;";
         document.getElementById("cssAlignment").innerText = cssCodeString;
     }
     else if (e.target.getAttribute("id")=="fullwidth"){
         navbarArea.style.justifyContent = "space-between";
-        cssCodeString = "justify-content : space-between;"
+        cssCodeString = "justify-content : space-between;";
         document.getElementById("cssAlignment").innerText = cssCodeString;
         listElements = document.querySelectorAll("#navbar-mockup-list li");
         for (i of listElements) {
@@ -144,7 +144,7 @@ function align (e) {
 // change font function
 
 function fontFamily(e) {
-    targetValue = e.target.value
+    targetValue = e.target.value;
     navbarArea.style.fontFamily = targetValue;
     cssCodeString = "font-family: " + targetValue;
     document.getElementById("cssfontFamily").innerText = cssCodeString;
@@ -155,17 +155,17 @@ function fontFamily(e) {
 function changeCase (e) {
     if(e.target.getAttribute("id")=="upper"){
         navbarArea.style.textTransform = "uppercase";
-        cssCodeString = "text-transform : uppercase;"
+        cssCodeString = "text-transform : uppercase;";
         document.getElementById("cssCase").innerText = cssCodeString;
     }
     else if (e.target.getAttribute("id")=="lower"){
         navbarArea.style.textTransform = "lowercase";
-        cssCodeString = "text-transform : lowercase;"
+        cssCodeString = "text-transform : lowercase;";
         document.getElementById("cssCase").innerText = cssCodeString;
     }
     else if (e.target.getAttribute("id")=="capitalize"){
         navbarArea.style.textTransform = "capitalize";
-        cssCodeString = "text-transform : capitalize;"
+        cssCodeString = "text-transform : capitalize;";
         document.getElementById("cssCase").innerText = cssCodeString;
     }
 }
@@ -174,14 +174,14 @@ function changeCase (e) {
 
 function fontsizechange(e) {
     document.querySelector("p > span").innerHTML = e.target.value;
-    cssCodeString = "font-size : "+e.target.value + " px;"
+    cssCodeString = "font-size : "+e.target.value + " px;";
     document.getElementById("cssfontSize").innerText = cssCodeString;
     navbarArea.style.fontSize = e.target.value + "px";
 }
 
 function opacitychange(e) {
     navbarArea.style.opacity = e.target.value + "%";
-    cssCodeString = "opacity: "+e.target.value + "% ;"
+    cssCodeString = "opacity: "+e.target.value + "% ;";
     document.getElementById("cssopacity").innerText = cssCodeString;
 }
 
@@ -190,7 +190,7 @@ function opacitychange(e) {
 function addNavItem(e) {
     e.preventDefault();
     if (navbarListNum < 5) {
-        newNavItem = document.getElementById('navbar-entries').value;
+        newNavItem = document.getElementById("navbar-entries").value;
         listItem = document.createElement("li");
         listItem.innerHTML = newNavItem;
         navbarArea.appendChild(listItem);
@@ -204,11 +204,11 @@ function addNavItem(e) {
         htmlCodeStringElement.innerText = htmlCodeString;
         attachDragListeners(htmlCodeStringElement);
         htmlCodeSection.appendChild(htmlCodeStringElement);
-        document.getElementById('navbar-entries').value = " ";
-        document.getElementById('navbar-entries').focus();
+        document.getElementById("navbar-entries").value = " ";
+        document.getElementById("navbar-entries").focus();
         navbarListNum++;
 } else {
-    alert("Maximum number of navbar entries is 5")
+    alert("Maximum number of navbar entries is 5");
 }
 }
 
@@ -218,7 +218,7 @@ function attachDragListeners(el) {
     el.addEventListener("dragstart",()=> dragStartId = el.id);
     // it is necessary to preventDefault on both enter and over drag events in order to allow a drop to take place.
     el.addEventListener("dragover", (e)=> e.preventDefault());
-    el.addEventListener("dragenter",(e)=> {e.preventDefault(); el.classList.add("dragover")});
+    el.addEventListener("dragenter",(e)=> {e.preventDefault(); el.classList.add("dragover");});
     el.addEventListener("dragleave",()=> el.classList.remove("dragover"));
     el.addEventListener("drop", updateNavItems);
 }
@@ -227,21 +227,21 @@ function updateNavItems(e) {
     //deletion drop process     
     e.target.classList.remove("dragover");   
     if(e.target.id == "trash"){
-        itemtoDelete = document.getElementById(`${dragStartId}`)
+        itemtoDelete = document.getElementById(`${dragStartId}`);
         // this is to delete the entry in the mockup area. Gets nodelist from the ul id of navbar-mockup-list
-        mockupitemtoDelete = document.querySelectorAll("#navbar-mockup-list li")
+        mockupitemtoDelete = document.querySelectorAll("#navbar-mockup-list li");
         itemtoDelete.remove();
         mockupitemtoDelete[dragStartId].remove();
         navbarListNum --;
-        reIndexEntries()
+        reIndexEntries();
     e.target.classList.remove("dragover");}
     //reordering drop process
     else {
-        let codeList = document.querySelectorAll("#navbar-code-html p")
+        let codeList = document.querySelectorAll("#navbar-code-html p");
         let changeTo = e.target.closest("p");
         let ctDuplicate = e.target.closest("p").innerHTML;
         let changeFrom = codeList[dragStartId];
-        changeTo.innerHTML = changeFrom.innerHTML
+        changeTo.innerHTML = changeFrom.innerHTML;
         changeFrom.innerHTML = ctDuplicate;
         reIndexEntries();
         reloadNavbarMockup(); 
@@ -250,7 +250,7 @@ function updateNavItems(e) {
 
 function reIndexEntries(){
     let iter = 0;
-    const listEntries = document.querySelectorAll("#navbar-code-html p")
+    const listEntries = document.querySelectorAll("#navbar-code-html p");
     for (i of listEntries){
         i.id = iter.toString();
         iter++;
@@ -263,7 +263,7 @@ function reloadNavbarMockup () {
     const codeList = document.querySelectorAll("#navbar-code-html p");
         for (i=0; i < mockupList.length; i++) {
             //removes the "li" tags embedded in the text of the p tag in the code area
-            clistOne = codeList[i].textContent.slice(4,-5)
+            clistOne = codeList[i].textContent.slice(4,-5);
             mockupList[i].innerHTML = clistOne;
         }
 }
@@ -276,7 +276,7 @@ function loadFontList(list) {
         newOption = document.createElement("option");
         newOption.innerHTML = i;
         newOption.setAttribute("value",i);
-        fontOptions.appendChild(newOption)
+        fontOptions.appendChild(newOption);
     }
 }
 
